d205be6dae2c19bbf1aeb5bcb481492d852f5368
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-21 17:48:29 +0200

    improve scripts handling

--- sv_block_social_links.php
+++ sv_block_social_links.php
@@ -60,19 +60,4 @@
 
 			return $this;
 		}
-		public function enqueue_scripts(): sv_block_social_links {
-			if(!$this->has_block_frontend('social-links')){
-				return $this;
-			}
-
-			if(!is_admin()){
-				$this->load_settings()->register_scripts();
-			}
-
-			foreach($this->get_scripts() as $script){
-				$script->set_is_enqueued();
-			}
-			
-			return $this;
-		}
 	}
