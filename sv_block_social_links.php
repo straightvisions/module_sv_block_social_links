5958a1d7406a0cc8e69a989776c96e3ba0308988
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-30 17:05:20 +0200

    remove default values

--- sv_block_social_links.php
+++ sv_block_social_links.php
@@ -28,7 +28,6 @@
 			$this->get_setting( 'font_size' )
 				->set_title( __( 'Font Size', 'sv100' ) )
 				->set_description( __( 'Font Size in pixel.', 'sv100' ) )
-				->set_default_value( 16 )
 				->set_is_responsive(true)
 				->load_type( 'number' );
 
